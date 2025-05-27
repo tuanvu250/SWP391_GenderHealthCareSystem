82559d05c39fb15f951d39d748b921295181ae46
tungbach12 <bachttse180220@fpt.edu.vn>
2025-05-27 20:25:19 +0700

    verr
    hi

---
+++ GenderHealthCareSystem/src/test/java/group4/genderhealthcaresystem/GenderHealthCareSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package group4.genderhealthcaresystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenderHealthCareSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
