95cfa7044ef851bfe0c4dcb2853ed97e2a651fce
huuanh20 <165425332+huuanh20@users.noreply.github.com>
2025-06-05 15:51:53 +0700

    update profile
    .

--- Backend/GenderHealthCare/src/main/java/GenderHealthCareSystem/controller/ProfileController.java
+++ Backend/GenderHealthCare/src/main/java/GenderHealthCareSystem/controller/ProfileController.java
@@ -24,6 +24,7 @@ public class ProfileController {
     @Autowired
    private UserRepository userRepository;
 
+
     @PutMapping("/me")
     public ResponseEntity<?> updateUserProfile(
             @AuthenticationPrincipal Jwt jwt,
@@ -47,6 +48,31 @@ public class ProfileController {
 
         return ResponseEntity.ok(Map.of("message", "User profile updated successfully!"));
     }
+
+
+//    @PutMapping("/me")
+//    public ResponseEntity<?> updateUserProfile(
+//            @AuthenticationPrincipal Jwt jwt,
+//            @RequestBody UserProfileResponse req
+//    ) {
+//        Integer userId = ((Number) jwt.getClaim("userID")).intValue();
+//        Optional<Users> optional = userRepository.findById(userId);
+//
+//        if (optional.isEmpty())
+//            return ResponseEntity.status(404).body(Map.of("message", "User not found!"));
+//
+//        Users user = optional.get();
+//        if (req.getFullName() != null) user.setFullName(req.getFullName());
+//        if (req.getGender() != null) user.setGender(req.getGender());
+//        if (req.getPhone() != null) user.setPhone(req.getPhone());
+//        if (req.getAddress() != null) user.setAddress(req.getAddress());
+//        if (req.getBirthDate() != null) user.setBirthDate(req.getBirthDate());
+//
+//        user.setUpdatedAt(java.time.LocalDateTime.now());
+//        userRepository.save(user);
+//
+//        return ResponseEntity.ok(Map.of("message", "User profile updated successfully!"));
+//    }
     
 
 }
