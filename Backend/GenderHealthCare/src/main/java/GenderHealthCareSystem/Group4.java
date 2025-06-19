4ca6c2afa182c33278550e2df7841044d8e347c4
thienhoibai <nguyendaothien2003@gmail.com>
2025-06-19 08:46:05 +0700

    them @

--- Backend/GenderHealthCare/src/main/java/GenderHealthCareSystem/Group4.java
+++ Backend/GenderHealthCare/src/main/java/GenderHealthCareSystem/Group4.java
@@ -2,9 +2,10 @@ package GenderHealthCareSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-
+@EnableScheduling
 public class Group4 {
 
     public static void main(String[] args) {
