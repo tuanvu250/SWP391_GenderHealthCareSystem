dfc11679ec0be83bad434de02da6b22d97565e72
huuanh20 <165425332+huuanh20@users.noreply.github.com>
2025-06-21 00:43:09 +0700

    update STI result

--- Backend/GenderHealthCare/src/main/java/GenderHealthCareSystem/repository/StisResultRepository.java
+++ Backend/GenderHealthCare/src/main/java/GenderHealthCareSystem/repository/StisResultRepository.java
@@ -2,6 +2,9 @@ package GenderHealthCareSystem.repository;
 
 import GenderHealthCareSystem.model.StisResult;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface StisResultRepository extends JpaRepository<StisResult, Integer> {
+    Optional<StisResult> findByStisBooking_BookingId(Integer bookingId);
+
 }
