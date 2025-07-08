8a5fcd7ff32a9308049d586e95c14160f41a6d67
tungbach12 <bachttse180220@fpt.edu.vn>
2025-07-08 21:05:36 +0700

    update user management

--- Backend/GenderHealthCare/src/main/java/GenderHealthCareSystem/controller/UserController.java
+++ Backend/GenderHealthCare/src/main/java/GenderHealthCareSystem/controller/UserController.java
@@ -1,19 +1,26 @@
 package GenderHealthCareSystem.controller;
 
+import GenderHealthCareSystem.dto.ApiResponse;
 import GenderHealthCareSystem.dto.ChangePasswordRequest;
 import GenderHealthCareSystem.dto.UserInfoResponse;
+import GenderHealthCareSystem.enums.AccountStatus;
 import GenderHealthCareSystem.model.Account;
 import GenderHealthCareSystem.model.Users;
 import GenderHealthCareSystem.service.AccountService;
 import GenderHealthCareSystem.service.UserService;
 import GenderHealthCareSystem.util.CustomUserDetails;
+import GenderHealthCareSystem.util.PageResponseUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.Map;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/api/users")
@@ -31,9 +38,12 @@ public class UserController {
     }
 
     @GetMapping("/{id}")
-    public Users getUserById(@PathVariable int id) {
+    public ResponseEntity<ApiResponse<?>> getUserById(@PathVariable int id) {
         // Logic to retrieve a user by ID
-        return userService.getUserById(id);
+       return new ResponseEntity<>(
+                new ApiResponse<>(HttpStatus.OK, "User found", userService.mapToResponse(userService.getUserById(id)), null),
+                HttpStatus.OK
+        );
     }
 
     @GetMapping("/me")
@@ -59,7 +69,32 @@ public class UserController {
                 user.getUserImageUrl(),
                 user.getBirthDate(),
                 user.getCreatedAt(),
-                user.getUpdatedAt());
+                user.getUpdatedAt(),
+                user.getProvider(),
+                user.getAccount().getAccountStatus());
+
+    }
+    @GetMapping("/Search")
+    public ResponseEntity<ApiResponse<?>> searchUser(@RequestParam(required = false) String name,
+                                                  @RequestParam(required = false) String email,
+                                                  @RequestParam(required = false) String phone,
+                                                  @RequestParam(defaultValue = "0") int page,
+                                                  @RequestParam(defaultValue = "5") int size,
+                                                  @RequestParam(required = false) LocalDateTime startDate,
+                                                  @RequestParam(required = false) LocalDateTime endDate,
+                                                  @RequestParam(required = false) String role,
+                                                  @RequestParam(required = false) AccountStatus status,
+                                                  @RequestParam(defaultValue = "createdAt") String sortBy,
+                                                  @RequestParam(defaultValue = "desc") String sort) {
+        Set<String> ALLOWED_SORT_FIELDS = Set.of(
+                "createdAt", "updatedAt", "fullName", "birthDate", "email", "phone"
+        );
+        if (!ALLOWED_SORT_FIELDS.contains(sortBy)) {
+            return ResponseEntity.badRequest().body(new ApiResponse<>(HttpStatus.BAD_REQUEST, "Invalid sort field, please use these fields: createdAt, updatedAt, fullName, birthDate, email, phone", null, "INVALID_SORT_FIELD"));
+        }
+        Page<UserInfoResponse> userInfoResponsePage= userService.searchUsers(name, email, phone, page, size, sortBy, sort, startDate, endDate, role, status);
+
+        return ResponseEntity.ok(new ApiResponse<>(HttpStatus.OK,userInfoResponsePage.getTotalElements() > 0 ? "Users found" : "No users found", PageResponseUtil.mapToPageResponse(userInfoResponsePage), null));
     }
 
     @PostMapping("/change-password")
--- Backend/GenderHealthCare/src/main/java/GenderHealthCareSystem/service/UserService.java
+++ Backend/GenderHealthCare/src/main/java/GenderHealthCareSystem/service/UserService.java
@@ -1,9 +1,18 @@
 package GenderHealthCareSystem.service;
 
+import GenderHealthCareSystem.dto.UserInfoResponse;
+import GenderHealthCareSystem.enums.AccountStatus;
+import GenderHealthCareSystem.model.BlogPost;
 import GenderHealthCareSystem.model.Users;
 import GenderHealthCareSystem.repository.UserRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Service
@@ -22,5 +31,37 @@ public class UserService {
         } else {
             throw new RuntimeException("User not found with id: " + id);
         }
+    }
+
+    public Page<UserInfoResponse> searchUsers(String name, String email, String phone, int page, int size, String sortBy, String sort, LocalDateTime startDate, LocalDateTime endDate, String role, AccountStatus status) {
+        Pageable pageable;
+        if ("asc".equalsIgnoreCase(sort)) {
+            pageable = PageRequest.of(page, size, Sort.by(sortBy).ascending());
+        } else {
+            pageable = PageRequest.of(page, size, Sort.by(sortBy).descending());
+        }
+
+        Page<Users> usersPage = userRepository.searchUsers(
+                name, email, phone, startDate, endDate,  status,role, pageable);
+        return usersPage.map(this::mapToResponse);
+    }
+    public UserInfoResponse mapToResponse(Users user) {
+        return new UserInfoResponse(
+                user.getUserId(),
+                user.getAccount().getAccountId(),
+                user.getAccount().getUserName(),
+                user.getAccount().getEmail(),
+                user.getRole().getRoleName(),
+                user.getFullName(),
+                user.getPhone(),
+                user.getGender(),
+                user.getAddress(),
+                user.getUserImageUrl(),
+                user.getBirthDate(),
+                user.getCreatedAt(),
+                user.getUpdatedAt(),
+                user.getProvider(),
+                user.getAccount().getAccountStatus()
+        );
     }
 }
