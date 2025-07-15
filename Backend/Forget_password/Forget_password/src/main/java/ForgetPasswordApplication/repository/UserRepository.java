a022a8f510e56aad291293d0ffa47c4c00637afe
huuanh20 <165425332+huuanh20@users.noreply.github.com>
2025-07-15 19:46:44 +0700

    delete file not use

--- Backend/Forget_password/Forget_password/src/main/java/ForgetPasswordApplication/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package ForgetPasswordApplication.repository;
-
-import ForgetPasswordApplication.model.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<Users,Integer> {
-}
